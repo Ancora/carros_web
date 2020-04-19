0767edfc5bc91cd3cced4788433e44bbc3ebfc92
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-19 11:37:44 -0300

    15-Nav Drawer.

--- lib/home.dart
+++ lib/home.dart
@@ -6,32 +6,57 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Size get size => MediaQuery.of(context).size;
+
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Miniatura de Carros ${size.width} / ${size.height}'),
       ),
       body: _body(),
+      drawer: Drawer(
+        child: _menu(),
+      ),
     );
   }
 
   _body() {
-    Size size = MediaQuery.of(context).size;
-
     return Row(
       children: <Widget>[
-        Container(
-          width: 230,
-          color: Colors.blue[100],
-        ),
-        Container(
-          width: size.width - 230,
-          color: Colors.black45,
-        ),
+        _menu(),
+        _right(),
       ],
+    );
+  }
+
+  _menu() {
+    return Container(
+      width: 230,
+      color: Colors.blue[100],
+      child: ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text('Item 1'),
+          ),
+          ListTile(
+            leading: Icon(Icons.star_border),
+            title: Text('Item 2'),
+          ),
+          ListTile(
+            leading: Icon(Icons.star_half),
+            title: Text('Item 3'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _right() {
+    return Container(
+      width: size.width - 230,
+      color: Colors.black45,
     );
   }
 }
