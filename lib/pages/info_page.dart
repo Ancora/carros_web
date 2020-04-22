3219b484b476bde91032787992f3cd5bc9055b14
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-21 23:03:59 -0300

    50-User Agent.

---
+++ lib/pages/info_page.dart
@@ -0,0 +1,76 @@
+import 'package:carrosweb/utils/web/web.dart';
+import 'package:flutter/material.dart';
+
+class InfoPage extends StatefulWidget {
+  @override
+  _InfoPageState createState() => _InfoPageState();
+}
+
+class _InfoPageState extends State<InfoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: ListView(
+        children: <Widget>[
+          _font(),
+          SizedBox(height: 20),
+          _userAgent(),
+        ],
+      ),
+    );
+  }
+
+  _font() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Text(
+              "Font Family: ${Theme.of(context).textTheme.bodyText1.fontFamily}"),
+          SizedBox(
+            height: 10,
+          ),
+          Text("Default  : Flutter Web"),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Roboto   : Flutter Web",
+            style: TextStyle(fontFamily: "Roboto"),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Mansalva : Flutter Web",
+            style: TextStyle(fontFamily: "Mansalva"),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Raleway  : Flutter Web",
+            style: TextStyle(fontFamily: "Raleway"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _userAgent() {
+    return Card(
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Text('UserAgent: [${userAgent()}]'),
+        ),
+      ),
+    );
+  }
+}
--- lib/web/menu.dart
+++ lib/web/menu.dart
@@ -3,6 +3,7 @@ import 'package:carrosweb/colors.dart';
 import 'package:carrosweb/pages/carros/carros_page.dart';
 import 'package:carrosweb/pages/count_page.dart';
 import 'package:carrosweb/pages/home_page.dart';
+import 'package:carrosweb/pages/info_page.dart';
 import 'package:carrosweb/pages/upload/upload_page.dart';
 import 'package:carrosweb/pages/usuarios_page.dart';
 import 'package:flutter/material.dart';
@@ -36,7 +37,7 @@ class _MenuState extends State<Menu> {
     menus.add(ItemMenu("Usuários", FontAwesomeIcons.userAlt, UsuariosPage()));
     menus.add(ItemMenu("Upload", FontAwesomeIcons.upload, UploadPage()));
     menus.add(ItemMenu("Storage", FontAwesomeIcons.database, CountPage()));
-    //menus.add(ItemMenu("Info", FontAwesomeIcons.info, InfoPage()));
+    menus.add(ItemMenu("Info", FontAwesomeIcons.info, InfoPage()));
   }
 
   @override
