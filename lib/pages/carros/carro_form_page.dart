35aa098656f7c05458973d8b18dfa02412b0926e
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-22 22:35:05 -0300

    60-lib\pages\carros\CarroFormPage e CarroPage.

---
+++ lib/pages/carros/carro_form_page.dart
@@ -0,0 +1,390 @@
+import 'package:carrosweb/imports.dart';
+
+class CarroFormPage extends StatefulWidget {
+  final Carro carro;
+
+  CarroFormPage({this.carro}) : super();
+
+  @override
+  _CarroFormPageState createState() => _CarroFormPageState();
+}
+
+class _CarroFormPageState extends State<CarroFormPage> {
+  // Url do upload
+  String urlFoto;
+
+  Carro get carro => widget.carro;
+
+  final GlobalKey<FormState> _formKey =
+      GlobalKey<FormState>(debugLabel: "carro_form");
+
+  var _showProgress = false;
+  final tNome = TextEditingController();
+  final tDesc = TextEditingController();
+  final tTipo = TextEditingController();
+  int _radioIndex = 0;
+
+  bool uploading = false;
+
+  final uploadHelper = UploadHelper();
+
+  StreamSubscription<UploadState> subscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Stream de upload
+    subscription = uploadHelper.stream.listen((UploadState state) {
+      _setUploadState(state);
+    });
+
+    // Copia os dados do carro para o form
+    if (carro != null) {
+      tNome.text = carro.nome;
+      tDesc.text = carro.descricao;
+      _radioIndex = getTipoInt(carro);
+    }
+  }
+
+  // Add validate email function.
+  String _validateNome(String value) {
+    if (value.isEmpty) {
+      return 'Informe o nome do carro.';
+    }
+
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BreadCrumb(
+      child: body(),
+      actions: carro != null
+          ? [
+              DeleteButton(
+                // widgets/delete_button.dart
+                onPressed: _onClickDelete,
+              )
+            ]
+          : null,
+    );
+  }
+
+  body() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: _cardFoto(),
+        ),
+        SizedBox(
+          width: 28,
+        ),
+        Expanded(
+          flex: 2,
+          child: _cardForm(),
+        )
+      ],
+    );
+  }
+
+  void _onClickTipo(int value) {
+    setState(() {
+      _radioIndex = value;
+    });
+  }
+
+  getTipoInt(Carro carro) {
+    switch (carro.tipo) {
+      case "classicos":
+        return 0;
+      case "esportivos":
+        return 1;
+      default:
+        return 2;
+    }
+  }
+
+  String _getTipo() {
+    switch (_radioIndex) {
+      case 0:
+        return "classicos";
+      case 1:
+        return "esportivos";
+      default:
+        return "luxo";
+    }
+  }
+
+  _cardFoto() {
+    return Card(
+      child: Container(
+        padding: EdgeInsets.all(16),
+//        width: 350,
+        height: 320,
+        child: Center(
+          child: ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              InkWell(
+                onTap: _onClickFoto,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    uploading
+                        ? Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : _foto(),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    Text(
+                      uploading
+                          ? "Aguarde o upload da foto..."
+                          : "Clique na imagem para tirar uma foto",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _foto() {
+    String url = carro != null ? carro.urlFoto : null;
+    if (urlFoto != null) {
+      // Se tirou foto, fica com a foto nova
+      url = urlFoto;
+    }
+
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxWidth: 250,
+        maxHeight: 250,
+      ),
+      child: url != null
+          ? Image.network(url)
+          : Image.asset("assets/imgs/car_logo.png", color: AppColors.bg),
+    );
+  }
+
+  _cardForm() {
+    return Card(
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: Form(
+          key: this._formKey,
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Tipo",
+//              textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: AppColors.bg,
+                  fontSize: 18,
+                ),
+              ),
+              _radioTipo(),
+              SizedBox(
+                height: 10,
+              ),
+              AppTextField(
+                // widgets/app_text_field.dart
+                label: "Nome",
+                controller: tNome,
+                required: true,
+                validator: _validateNome,
+                keyboardType: TextInputType.text,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              AppTextField(
+                label: "Descrição",
+                controller: tDesc,
+                keyboardType: TextInputType.text,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  AppButton(
+                    // widgets/app_button.dart
+                    "Cancelar",
+                    onPressed: _onClickCancelar,
+                    whiteMode: true,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  AppButton(
+                    "Salvar",
+                    onPressed: _onClickSalvar,
+                    showProgress: _showProgress,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _radioTipo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Radio(
+          value: 0,
+          groupValue: _radioIndex,
+          onChanged: _onClickTipo,
+        ),
+        Text(
+          "Clássicos",
+          style: TextStyle(color: AppColors.texto, fontSize: 14),
+        ),
+        Radio(
+          value: 1,
+          groupValue: _radioIndex,
+          onChanged: _onClickTipo,
+        ),
+        Text(
+          "Esportivos",
+          style: TextStyle(color: AppColors.texto, fontSize: 14),
+        ),
+        Radio(
+          value: 2,
+          groupValue: _radioIndex,
+          onChanged: _onClickTipo,
+        ),
+        Text(
+          "Luxo",
+          style: TextStyle(color: AppColors.texto, fontSize: 14),
+        ),
+      ],
+    );
+  }
+
+  _onClickFoto() {
+    uploadHelper.upload();
+  }
+
+  void _setUploadState(UploadState state) {
+    if (state.started) {
+      print("Upload iniciado...");
+      setState(() {
+        uploading = true;
+      });
+    } else {
+      upload(state.fileUpload);
+    }
+  }
+
+  upload(FileUpload file) async {
+    ApiResponse<String> response =
+        await UploadApi.upload(file.fileName, file.mimeType, file.base64);
+
+    print("Upload ok: ${response.result}");
+
+    if (response.ok) {
+      String url = response.result;
+      setState(() {
+        uploading = false;
+        this.urlFoto = url;
+      });
+    }
+  }
+
+  _onClickSalvar() async {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+
+    // Cria o carro
+    var c = carro ?? Carro();
+    c.nome = tNome.text;
+    c.descricao = tDesc.text;
+    c.tipo = _getTipo();
+
+    if (urlFoto != null) {
+      // Atualiza a URL de upload no carro
+      c.urlFoto = urlFoto;
+    }
+
+    print("Carro: $c");
+
+    setState(() {
+      _showProgress = true;
+    });
+
+    print("Salvar o carro $c");
+
+    ApiResponse response =
+        await CarrosApi.save(context, c); // utils/api_response.dart
+
+    if (response.ok) {
+      alert(context, "Carro salvo com sucesso", callback: () {
+//        EventBus.get(context).sendEvent(CarroEvent("carro_salvo",c.tipo));
+
+        PagesModel.get(context).pop();
+      });
+    } else {
+      alert(context, response.msg);
+    }
+
+    setState(() {
+      _showProgress = false;
+    });
+
+    print("Fim.");
+  }
+
+  _onClickCancelar() {
+    PagesModel.get(context).pop();
+  }
+
+  void _onClickDelete() {
+    // Alerta para confirmar
+    alertConfirm(context, "Deseja mesmo excluir?", confirmCallback: delete);
+  }
+
+  delete() async {
+    ApiResponse response = await CarrosApi.delete(context, carro);
+    if (response.ok) {
+      alert(context, response.msg, callback: () {
+        PagesModel.get(context).pop();
+      });
+    } else {
+      alert(context, response.msg);
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    subscription.cancel();
+    uploadHelper.dispose();
+  }
+}
--- lib/pages/carros/carro_page.dart
+++ lib/pages/carros/carro_page.dart
@@ -1,9 +1,65 @@
-import 'package:carrosweb/app_model.dart';
-import 'package:carrosweb/pages/carros/carro.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:carrosweb/imports.dart';
 
-class CarroPage extends StatefulWidget {
+class CarroPage extends StatelessWidget {
+  final Carro car;
+  CarroPage(this.car);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(car.nome ?? ''),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.place),
+            onPressed: () => _onClickMapa(context),
+          ),
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: () => _onClickVideo(context),
+          ),
+        ],
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          car.urlFoto != null
+              ? Image.network(car.urlFoto)
+              : FlutterLogo(
+                  size: 100,
+                )
+        ],
+      ),
+    );
+  }
+
+  void _onClickMapa(context) {
+    if (car.latitude != null && car.longitude != null) {
+      launch(// web\utils\web.dart
+          "https://www.google.com/maps/@${car.latitude},${car.longitude},19z");
+    } else {
+      alert(context, "Este carro não possui Lat/Lng da fábrica.");
+    }
+  }
+
+  void _onClickVideo(context) {
+    if (car.urlVideo != null && car.urlVideo.isNotEmpty) {
+      launch(car.urlVideo);
+    } else {
+      alert(context, "Este carro não possui nenhum vídeo");
+    }
+  }
+}
+
+/* class CarroPage extends StatefulWidget {
   final Carro car;
   CarroPage(this.car);
 
@@ -54,3 +110,4 @@ class _CarroPageState extends State<CarroPage> {
     app.pop();
   }
 }
+ */
