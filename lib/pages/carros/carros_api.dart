edd382efa55e045716ab70ac954c1037dcdfb4e8
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-22 22:47:23 -0300

    60-lib\pages\carros\Carro e CarrosApi.

--- lib/pages/carros/carros_api.dart
+++ lib/pages/carros/carros_api.dart
@@ -1,18 +1,22 @@
 import 'dart:convert' as convert;
-import 'package:http/http.dart' as http;
-import 'package:carrosweb/pages/carros/carro.dart';
+import 'package:carrosweb/imports.dart';
+import 'package:carrosweb/utils/http_helper.dart' as http;
+
+class TipoCarro {
+  static final String classicos = "classicos";
+  static final String esportivos = "esportivos";
+  static final String luxo = "luxo";
+}
 
 class CarrosApi {
-  static Future<List<Carro>> getCarros(context) async {
-    String url = "https://carros-springboot.herokuapp.com/api/v1/carros";
+  static Future<List<Carro>> getCarros(context, {String tipo}) async {
+    String url = "$BASE_URL/carros";
 
-    print("GET > $url");
+    if (tipo != null) {
+      url += '/tipo/$tipo';
+    }
 
-    final response = await http.get(url);
-
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-
+    final response = await http.get(url); // utils/http_helper.dart
     String json = response.body;
 
     List list = convert.json.decode(json);
@@ -20,5 +24,42 @@ class CarrosApi {
     List<Carro> carros = list.map<Carro>((map) => Carro.fromMap(map)).toList();
 
     return carros;
+  }
+
+  static Future<ApiResponse> save(context, Carro car) async {
+    try {
+      String url = "$BASE_URL/carros";
+      String json = car.toJson();
+
+      final response = await http.post(context, url, body: json);
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        Map mapResponse = convert.json.decode(response.body);
+        Carro carro = Carro.fromMap(mapResponse);
+        print("Carro salvo: ${carro.id}");
+        return ApiResponse.ok();
+      }
+
+      Map mapResponse = convert.json.decode(response.body);
+      return ApiResponse.error(msg: mapResponse["error"]);
+    } catch (e) {
+      print(e);
+      return ApiResponse.error(msg: "Não foi possível salvar o carro");
+    }
+  }
+
+  static Future<ApiResponse> delete(context, Carro car) async {
+    try {
+      String url = "$BASE_URL/carros/${car.id}";
+      var response = await http.delete(context, url);
+
+      if (response.statusCode == 200) {
+        Map mapResponse = convert.json.decode(response.body);
+        return ApiResponse.ok(msg: mapResponse["msg"]);
+      }
+    } catch (e) {
+      print(e);
+    }
+    return ApiResponse.error(msg: "Não foi possível deletar o carro");
   }
 }
