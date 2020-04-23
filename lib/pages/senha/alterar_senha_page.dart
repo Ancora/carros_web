98bd1cfcb12474c253786aba631c189f7cc4cbab
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-23 12:45:02 -0300

    60-\lib\pages\senha\*.dart.

---
+++ lib/pages/senha/alterar_senha_page.dart
@@ -0,0 +1,71 @@
+import 'package:carrosweb/imports.dart';
+
+class AlterarSenhaPage extends StatefulWidget {
+  @override
+  _AlterarSenhaPageState createState() => _AlterarSenhaPageState();
+}
+
+class _AlterarSenhaPageState extends State<AlterarSenhaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FormTemplate(
+        "Alterar Senha",
+        _form(),
+      ),
+    );
+  }
+
+  _form() {
+    return Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          AppTextField(
+            label: "Senha Atual",
+            width: 540,
+            required: true,
+            password: true,
+          ),
+          AppTextField(
+            label: "Nova Senha",
+            width: 540,
+            required: true,
+            password: true,
+          ),
+          AppTextField(
+            label: "Confirmar senha",
+            width: 540,
+            required: true,
+            password: true,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              AppButton(
+                "Cancelar",
+                onPressed: _onClickCancelar,
+                whiteMode: true,
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              AppButton("Alterar", onPressed: _onClickAlterarSenha),
+              SizedBox(
+                width: 20,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  _onClickAlterarSenha() {
+    alert(context, "Não implementado :-)");
+  }
+
+  _onClickCancelar() {
+    PagesModel.get(context).pop();
+  }
+}
---
+++ lib/pages/senha/esqueci_senha_page.dart
@@ -0,0 +1,84 @@
+import 'package:carrosweb/imports.dart';
+import 'package:carrosweb/widgets/card_form.dart';
+
+class EsqueciSenhaPage extends StatefulWidget {
+  @override
+  _EsqueciSenhaPageState createState() => _EsqueciSenhaPageState();
+}
+
+class _EsqueciSenhaPageState extends State<EsqueciSenhaPage> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        color: Color.fromARGB(255, 92, 107, 128),
+        width: size.width,
+        height: size.height,
+        child: CardForm(
+          title: "Esqueci a Senha",
+          child: _form(),
+        ),
+      ),
+    );
+  }
+
+  /* _layout2() {
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        Image.asset(
+          "assets/imgs/background.jpg",
+          fit: BoxFit.fill,
+          width: double.infinity,
+        ),
+        CardForm(
+          title: "Esqueci a Senha",
+          child: _form(),
+        )
+      ],
+    );
+  } */
+
+  _form() {
+    return ListView(
+      shrinkWrap: true,
+      children: <Widget>[
+        Container(
+          child: AppTextField(
+            label: "Login/Email",
+            required: true,
+          ),
+        ),
+        Row(
+          children: <Widget>[
+            Spacer(),
+            AppButton(
+              "Cancelar",
+              onPressed: _onClickCancelar,
+              whiteMode: true,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            AppButton("Enviar", onPressed: _onClickEsqueciSenha),
+            SizedBox(
+              width: 20,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  _onClickCancelar() {
+    pop(context);
+  }
+
+  _onClickEsqueciSenha() {
+    alert(context, "Não implementado :-)", callback: () {
+      pop(context);
+    });
+  }
+}
