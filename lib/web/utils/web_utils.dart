d2bff1aff6ebe8eada1e55b1aae913bc8df3bb05
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-22 23:29:18 -0300

    60-web\utils\Prefs, WebUtils e Web.

---
+++ lib/web/utils/web_utils.dart
@@ -0,0 +1,20 @@
+import 'package:carrosweb/imports.dart';
+
+fontSize(double value, {double min = 8, double max = 22}) {
+  return size(value, min: min, max: max);
+}
+
+size(double value, {double min = 0, double max = double.infinity}) {
+  if (value < min) {
+    return min;
+  }
+  if (value > max) {
+    return max;
+  }
+  return value;
+}
+
+smallScreen(context) {
+  Size size = MediaQuery.of(context).size;
+  return size.width <= 500;
+}
