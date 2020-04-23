ad7482ad330c06f2197d27d6c604b5a881224a67
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-23 12:41:10 -0300

    60-\lib\pages\push\*.dart.

---
+++ lib/pages/push/push_input.dart
@@ -0,0 +1,62 @@
+import 'dart:convert' as convert;
+
+import 'package:carrosweb/imports.dart';
+import 'package:http/http.dart' as http;
+
+class PushInput {
+  // Firebase key
+  String key;
+  String title;
+  String msg;
+  String token;
+
+  @override
+  String toString() {
+    return 'PushInput{key: $key, title: $title, msg: $msg, token: $token}';
+  }
+}
+
+class PushApi {
+  static Future<ApiResponse> push(PushInput input) async {
+    final url = "https://fcm.googleapis.com/fcm/send";
+
+    final params = {
+      "notification": {
+        "title": input.title,
+        "body": input.msg,
+        "priority": "high"
+      },
+      "data": {
+        "click_action": "FLUTTER_NOTIFICATION_CLICK",
+        "msg": "Firebase muito legal"
+      },
+      "registration_ids": [input.token]
+    };
+
+    final headers = {
+      "Content-Type": "application/json",
+      "Authorization":
+          "Key=${input.key}"
+    };
+
+    print("> Push POST: $url");
+
+    String json = convert.json.encode(params);
+
+    print("> $params");
+
+    final response = await http.post(
+      url,
+      body: json,
+      headers: headers,
+    );
+
+    print("< ${response.body}");
+
+    if (response.statusCode == 200) {
+      return ApiResponse.ok(msg: "Push enviado com sucesso.");
+    }
+
+    return ApiResponse.error(msg: "Erro ao enviar push");
+  }
+}
---
+++ lib/pages/push/push_page.dart
@@ -0,0 +1,96 @@
+import 'package:carrosweb/imports.dart';
+import 'package:carrosweb/pages/push/push_input.dart';
+import 'package:carrosweb/utils/alert.dart';
+import 'package:carrosweb/web/breadcrumb.dart';
+import 'package:flutter/material.dart';
+
+class PushPage extends StatefulWidget {
+  PushPage();
+
+  @override
+  _PushPageState createState() => _PushPageState();
+}
+
+class _PushPageState extends State<PushPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _input = PushInput();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BreadCrumb(child: _body()),
+    );
+  }
+
+  // Add validate email function.
+  String _validateRequired(String value, String msg) {
+    if (value.isEmpty) {
+      return msg;
+    }
+
+    return null;
+  }
+
+  _body() {
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                "Exemplo de enviar Push Notification usando o Firebase Messaging (FCM)",
+              ),
+            ),
+            SizedBox(height: 26),
+            AppTextField(
+              label:
+                  "Server Key (Console do Firebase > Project Settings > Cloud Messaging > Server Key)",
+              onChanged: (String s) => _input.key = s,
+              validator: (String s) =>
+                  _validateRequired(s, "Informe a chave do Firebase"),
+            ),
+            AppTextField(
+              label: "Titulo",
+              onChanged: (String s) => _input.title = s,
+              validator: (String s) => _validateRequired(s, "Informe o título"),
+            ),
+            AppTextField(
+              label: "Mensagem / body",
+              onChanged: (String s) => _input.msg = s,
+              validator: (String s) =>
+                  _validateRequired(s, "Informe a mensagem"),
+            ),
+            AppTextField(
+              label: "Firebase Token (token do celular)",
+              onChanged: (String s) => _input.token = s,
+              validator: (String s) =>
+                  _validateRequired(s, "Informe a mensagem"),
+            ),
+            Center(
+              child: AppButton(
+                "Enviar Push",
+                onPressed: _onClickPush,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _onClickPush() async {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+
+    print("> $_input");
+
+    final response = await PushApi.push(_input);
+
+    alert(context, response.msg);
+  }
+}
