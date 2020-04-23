a7e41932ac23af03eba8311df7386c65de704711
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-23 00:50:55 -0300

    60-widgets\FormTemplate e MaterialContainer.

---
+++ lib/widgets/form_template.dart
@@ -0,0 +1,37 @@
+import 'package:carrosweb/colors.dart';
+import 'package:flutter/material.dart';
+
+class FormTemplate extends StatelessWidget {
+  final String title;
+  final Widget form;
+
+  FormTemplate(this.title, this.form);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.bg,
+      width: double.infinity,
+      padding: EdgeInsets.all(20),
+      child: ListView(
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(fontSize: 22, color: AppColors.texto),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: form,
+          )
+        ],
+      ),
+    );
+  }
+}
